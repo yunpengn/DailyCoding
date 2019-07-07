80e54388d6e3984037d3d2dc58fa61207b2ee142
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-07-07 17:37:21 +0800

    Add two sum

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,2 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoSum {
+    public boolean haveSum(int[] arr, int target) {
+        Set<Integer> items = new HashSet<>();
+
+        for (int n: arr) {
+            if (items.contains(target - n)) {
+                return true;
+            }
+            items.add(n);
+        }
+
+        return false;
+    }
 }
---
+++ src/test/java/TwoSumTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class TwoSumTest {
+    private TwoSum tester = new TwoSum();
+
+    @Test
+    void test() {
+        boolean result = tester.haveSum(new int[]{10, 5, 7}, 17);
+        assertTrue(result);
+
+        result = tester.haveSum(new int[]{10, 5, 7}, 11);
+        assertFalse(result);
+    }
+
+    @Test
+    void duplicate() {
+        boolean result = tester.haveSum(new int[]{7, 5, 7}, 14);
+        assertTrue(result);
+
+        result = tester.haveSum(new int[]{7, 5, 7}, 12);
+        assertTrue(result);
+
+        result = tester.haveSum(new int[]{7, 5, 7}, 11);
+        assertFalse(result);
+    }
+
+    @Test
+    void empty() {
+        boolean result = tester.haveSum(new int[]{}, 14);
+        assertFalse(result);
+    }
+}
